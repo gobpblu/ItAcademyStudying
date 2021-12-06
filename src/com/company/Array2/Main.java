0824b59b9bedd42095ab811109dcbefa389f99b6
gobpo2002 <gobpo2002@gmail.com>
2021-12-06 16:22:21 +0600

    Second change

--- src/com/company/Array2/Main.java
+++ src/com/company/Array2/Main.java
@@ -324,6 +324,7 @@ public class Main {
             if (nums[i] == 2 && nums[i + 1] != 2 && nums[i - 1] != 2) return false;
         }
         if (nums.length > 1 && nums[nums.length - 1] == 2 && nums[nums.length - 2] != 2) return false;
+        else if (nums.length == 1 && nums[0] == 2) return false;
         return true;
     }
 }
